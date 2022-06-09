f1e792a924d9c9154b1993972a0dbd8a9f1c1934
Enrik <Enrik.n1rvan@gmail.com>
2022-06-09 14:42:31 +0300

    add all folders and file gitignore

---
+++ Quest1_Big_num/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if (A < B) {
+    Console.WriteLine($"max = {B}");
+} else {
+    Console.WriteLine($"max = {A}");
+}
---
+++ Quest2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if (A < B) {
+    Console.WriteLine($"max = {B}");
+} else if (A < C) {
+    Console.WriteLine($"max = {C}");
+} else if (B < A) {
+    Console.WriteLine($"max = {A}");
+} else if (B < C) {
+    Console.WriteLine($"max = {C}");
+} else if (C < A) {
+    Console.WriteLine($"max = {A}");
+} else if (C < B) {
+    Console.WriteLine($"max = {B}");
+}
