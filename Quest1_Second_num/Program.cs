3f87ccbce002df21b4b3725ced972ae9d31aa8d0
Enrik <Enrik.n1rvan@gmail.com>
2022-06-14 23:01:31 +0300

    add quest 1 second num

---
+++ Quest1_Second_num/Program.cs
@@ -0,0 +1,14 @@
+﻿// int Number = new Random().Next(100,1000);
+// Console.WriteLine(Number);
+
+// int num = Number / 10 % 10;
+
+// Console.WriteLine(num);
+
+
+int Number = new Random().Next(100,1000);
+Console.WriteLine(Number);
+
+char[] arr = Number.ToString().ToArray();
+
+Console.WriteLine(arr[1]);
