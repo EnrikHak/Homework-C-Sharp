1cbab964626695865bd4be1f5db2c3246375fe9b
Enrik <Enrik.n1rvan@gmail.com>
2022-06-14 23:02:26 +0300

    add quest 3 third num

---
+++ Quest3_Third_num/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// char[] Array = num.ToString().ToCharArray();
+
+// for (int i = 0; i < Array.Length; i++) {
+
+//     if (Array.Length >= i) {
+//         Console.WriteLine(Array[2]);
+//         break;
+//     } else if (Array.Length < i) {
+//         Console.WriteLine("Третьей цифры нет");
+//     };
+// }
+
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num <= 999) {
+    Console.WriteLine(num % 10);
+} else if (num > 999 && num <= 9999) {
+    Console.WriteLine(num / 10 % 10);
+} else if (num > 9999 && num <= 99999) {  // и так далее...
+    Console.WriteLine(num / 100 % 10);    // деление каждый раз увеличивается в 10 раз
+} else {
+    Console.WriteLine("Нет третьего числа");
+}
