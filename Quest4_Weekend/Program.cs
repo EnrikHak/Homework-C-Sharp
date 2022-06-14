96d9c0b0ce49c8ff58395b7af8512b747e63bae1
Enrik <Enrik.n1rvan@gmail.com>
2022-06-14 23:02:44 +0300

    add quest 4 weekend

---
+++ Quest4_Weekend/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 5) {
+    Console.WriteLine("NO! It's working day");
+} else if (number > 7) {
+    Console.WriteLine("Error!!!");
+} else {
+    Console.WriteLine("Yes!!! Weekend!");
+}
