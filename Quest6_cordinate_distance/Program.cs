02f5ae010a7f85f3f0abe1a141269c177c017b60
Enrik <Enrik.n1rvan@gmail.com>
2022-06-16 17:33:26 +0300

    add quest 6 distance

---
+++ Quest6_cordinate_distance/Program.cs
@@ -0,0 +1,24 @@
+﻿void Distance (int x, int x1, int y, int y1, int z, int z1) {
+    double distance = Math.Sqrt((Math.Pow(x1 - x)) + (Math.Pow(y1 - y)) + (Math.Pow(z1 - z)));
+    Console.WriteLine($"Расстояние: {distance}");
+}
+
+Console.WriteLine("Введите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Distance(x, x1, y, y1, z, z1);
