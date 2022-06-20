c3bf401900c5c5ff576bb2ffb0604b8692af6a2c
Enrik <Enrik.n1rvan@gmail.com>
2022-06-20 16:43:28 +0300

    add quest 8 stepen chisla ot dugogo chisla

---
+++ Quest8_stepenChislaAotB/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+void stepen (int A, int B) {
+    int result = 1;
+    for (int i = 1; i <= B; i++) {
+        result = result * A;
+    }    
+    Console.WriteLine($"{result}");
+}
+
+stepen(A, B);
+
+
+//               ВАРИАНТ 2
+
+// Console.WriteLine("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// void stepen (int A, int B) {
+//     double result = Math.Pow(A,B);
+//     Console.WriteLine($"{result}");
+// }
+
+// stepen(A, B);
