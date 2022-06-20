b14b9cb76fcef52b1ef821d6a165c817b3443620
Enrik <Enrik.n1rvan@gmail.com>
2022-06-20 16:42:14 +0300

    add quest 10 array8

---
+++ Quest10_array8/Program.cs
@@ -0,0 +1,18 @@
+﻿void Massiv(int[] arr) {
+    
+    for (int i = 0; i < arr.Length; i++) {
+        Console.WriteLine("Введите элемент");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PushArray(int[] arr){
+     for (int i = 0; i < arr.Length; i++) {
+        Console.Write($"{arr[i]}");
+    }
+}
+
+int[] nums = new int[8];
+
+Massiv(nums);
+PushArray(nums);
