eb757e459cfdc1b6f5f5c47c2cf04fa8dd3c78bb
Enrik <Enrik.n1rvan@gmail.com>
2022-07-01 12:20:33 +0300

    add quest 17 numbers Fibonacci

---
+++ Quest17_numberOfFibonaci/Program.cs
@@ -0,0 +1,20 @@
+﻿int Fibonaci() {
+    
+    Console.WriteLine("Введите число до которого нужно считать: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int a = 0;
+    int b = 1;
+
+    for (int i = 0; i < n; i++) {
+        int sum = a;
+        a = b;
+        b = sum + b;
+        Console.Write($"{b} ");
+    }
+    return b;
+}
+
+
+
+Fibonaci();
