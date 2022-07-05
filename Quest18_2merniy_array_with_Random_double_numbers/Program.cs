7e519f3a11354c4388a1832daef932b138f37a38
Enrik <Enrik.n1rvan@gmail.com>
2022-07-05 21:33:14 +0300

    add quest 18 multi array with random numbers

---
+++ Quest18_2merniy_array_with_Random_double_numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿void FillArray(double[,] array) {
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i, j] = (new Random().NextDouble()) * (new Random().Next(-10,10));
+        }
+    }
+}
+
+void PrintArray(double[,] array) {
+
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{Math.Round(array[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateArray(int m, int n) {
+    return new double [m, n];
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = CreateArray(m, n);
+
+FillArray(arr);
+PrintArray(arr);
