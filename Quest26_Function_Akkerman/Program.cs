70283e6bcd454e30b8f925f65f55c7c19cecb5af
Enrik <Enrik.n1rvan@gmail.com>
2022-07-11 01:28:14 +0300

    add quest 26 Function Akkerman

---
+++ Quest26_Function_Akkerman/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Input number M to start: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number N to finish: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int FunctionAkkerman(int M, int N) {
+
+    if(M == 0) {
+        return N + 1;
+    }
+    if(M > 0 && N == 0) {
+        return FunctionAkkerman(M - 1, 1);
+    }
+    if(M > 0 && N > 0) {
+        return FunctionAkkerman(M - 1,FunctionAkkerman(M, N - 1));
+    }
+    return 0;
+}
+int result = FunctionAkkerman(M, N);
+Console.WriteLine(result);
+FunctionAkkerman(M, N);
