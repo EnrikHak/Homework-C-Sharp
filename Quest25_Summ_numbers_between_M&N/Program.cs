c0803a5ed3520001a37247a8cfb09b227cad7c52
Enrik <Enrik.n1rvan@gmail.com>
2022-07-11 01:27:38 +0300

    add quest 25 summ numbers between m&N

---
+++ Quest25_Summ_numbers_between_M&N/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Input number M to start: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number N to finish: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// void SummOfNum(int M, int N) {
+//     int first = M;
+//     int last = N;
+//     if(first > last) {
+//         Console.WriteLine("Error! Element M more then element N!");
+//     } else {
+//         int sum = 0;
+//         for(int i = first; i <= last; i++) {
+//             sum = sum + i;
+//         }
+//         Console.WriteLine(sum);
+//     }
+// }
+// SummOfNum(M, N);
+
+
+int SummOfNum(int M, int N) {
+
+    if(M < N) {
+
+        M += SummOfNum(M + 1, N);
+    }
+    return M;
+}
+Console.WriteLine(SummOfNum(M, N));
+SummOfNum(M, N);
